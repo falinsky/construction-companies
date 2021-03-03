93674b2c66988d9c6b6642cc8ee3f49bdebb6cc8
falinsky <sergey@falinsky.com>
2021-03-03 17:44:16 +0200

    Convert testUtils to tsx and add typings
    Update according to the official recommendations for custom render (https://testing-library.com/docs/react-testing-library/setup/#custom-render)

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
+import { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
-const Providers = ({ children }) => {
-  return children
+const Providers: FC = ({ children }) => {
+  return <>{children}</>
   // return (
   //   <ThemeProvider theme="light">
   //     <TranslationProvider messages={defaultStrings}>
@@ -14,8 +15,10 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui, options = {}) =>
-  render(ui, { wrapper: Providers, ...options })
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+) => render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
