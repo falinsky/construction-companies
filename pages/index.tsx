a13717305cd97f2c4d6144c498d69bd5c565e829
falinsky <sergey@falinsky.com>
2021-03-05 20:45:27 +0200

    Show companies data on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import Head from 'next/head'
+import { useQuery } from 'react-query'
+import { getCompanies } from '../lib/api-client/client'
+import CompaniesDataGrid from '../components/CompaniesDataGrid'
+import { Typography } from '@material-ui/core'
 
 export const Home = (): JSX.Element => {
+  const { data, isLoading, isError } = useQuery('companies', () =>
+    getCompanies()
+  )
+
   return (
-    <Head>
-      <title>Construction Companies</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>Construction Companies</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Typography variant="h1">Construction Companies</Typography>
+        <CompaniesDataGrid
+          companies={data}
+          isLoading={isLoading}
+          isError={isError}
+        />
+      </main>
+    </>
   )
 }
 
